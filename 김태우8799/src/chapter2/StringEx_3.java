47cc1c7c6ffd3ee3afa80854c5cbb10667a96b16
ITPS <ITPS@DESKTOP-NUOPUIF>
2021-05-14 16:13:44 +0900

    test2

--- 김태우8799/src/chapter2/StringEx_3.java
+++ 김태우8799/src/chapter2/StringEx_3.java
@@ -4,8 +4,10 @@ public class StringEx_3 {
 //문자열의 형변환 , 
 //ㅡㅡㅡㅡㅡJSP에서 많이쓴다.ㅡㅡㅡㅡㅡ , 인터넷 홈페이지 로그인창이 예시임.
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-'generated method stub
+		char a = 'b';
+		System.out.println(a);
+		
 	}
 
 }
